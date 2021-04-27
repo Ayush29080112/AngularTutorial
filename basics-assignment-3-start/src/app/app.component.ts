17e28fe8489f0a596da9344257339f4495869b92
System Administrator <ayushbajpai1401@gmail.com>
2021-04-27 23:21:24 +0530

    Added basic assignment

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  displayParagraph:boolean = false;
+  clickEvent = [];
+  i = 0 ;
+  onButtonClick(){
+    this.i = this.i+1;
+    this.clickEvent.push(this.i);
+    this.displayParagraph = !this.displayParagraph;
+  }
+
+  getBackGroundColor(){
+    return this.i > 4? 'blue' :'white'
+  }
+
+  applyClass(){
+    return this.i > 4? true:false
+  }
 }
